895c3e734205365b961857503b76e0e121a068b5
Paulo Henrique <ph.1928@hotmail.com>
2024-09-17 22:54:53 -0300

    feat: DbConnect for database connection

---
+++ src/main/java/db/DbConnect.java
@@ -0,0 +1,30 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnect {
+
+    static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String url = "jdbc:postgresql://localhost:5432/songs";
+            String user = "admin";
+            String password = "admin";
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class error: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getMessage());
+            System.out.println("Error: " + e.getErrorCode());
+        }
+
+        return connection;
+    }
+
+}
