0aaec33b87447e278b9727e2d9b1ee2a85954650
Paulo Henrique <ph.1928@hotmail.com>
2024-09-18 21:57:22 -0300

    refactor: remove durationInSeconds;

--- src/main/java/model/Song.java
+++ src/main/java/model/Song.java
@@ -4,14 +4,12 @@ public class Song {
     private int id;
     private String title;
     private String artistName;
-    private int durationInSeconds;
     private String genre;
 
-    public Song(int id, String title, String artistName, int durationInSeconds, String genre) {
+    public Song(int id, String title, String artistName, String genre) {
         this.id = id;
         this.title = title;
         this.artistName = artistName;
-        this.durationInSeconds = durationInSeconds;
         this.genre = genre;
     }
 
@@ -39,14 +37,6 @@ public class Song {
 
     public void setArtistName(String artistName) {
         this.artistName = artistName;
-    }
-
-    public int getDurationInSeconds() {
-        return durationInSeconds;
-    }
-
-    public void setDurationInSeconds(int durationInSeconds) {
-        this.durationInSeconds = durationInSeconds;
     }
 
     public String getGenre() {
