f8e7f77e3753631e7bcbef83fde2b76c4a5b91df
Paulo Henrique <ph.1928@hotmail.com>
2024-09-20 16:15:57 -0300

    feat: handle rendering song edit form

--- src/main/java/web/SongServlet.java
+++ src/main/java/web/SongServlet.java
@@ -1,6 +1,5 @@
 package web;
 
-import dao.SongDao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,6 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Song;
+import service.SongService;
 
 import java.io.IOException;
 
@@ -16,32 +16,46 @@ public class SongServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    private SongDao songDao;
+    private final SongService songService;
 
     public SongServlet() {
         super();
-        this.songDao = new SongDao();
+        this.songService = new SongService();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("songId") != null) {
+            String songId = req.getParameter("songId");
+            Song song = this.songService.getSongById(Integer.parseInt(songId));
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher("edit-song.jsp");
+            req.setAttribute("song", song);
+            dispatcher.forward(req, resp);
+            return;
+        }
+
         RequestDispatcher dispatcher = req.getRequestDispatcher("songs.jsp");
-        req.setAttribute("songs", this.songDao.getAllSongs());
-        System.out.println("Songs in servlet: " + req.getAttribute("songs"));
+        req.setAttribute("songs", this.songService.getAllSongs());
         dispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Song song = new Song();
-        song.setTitle(req.getParameter("title"));
-        song.setArtistName(req.getParameter("artistName"));
-        song.setGenre(req.getParameter("genre"));
-
-        this.songDao.addSong(song);
-
         RequestDispatcher dispatcher = req.getRequestDispatcher("songs.jsp");
-        req.setAttribute("songs", this.songDao.getAllSongs());
+
+        try {
+            Song song = new Song();
+            song.setTitle(req.getParameter("title"));
+            song.setArtistName(req.getParameter("artistName"));
+            song.setGenre(req.getParameter("genre"));
+
+            this.songService.addSong(song);
+        } catch (Exception e) {
+            req.setAttribute("error", "All fields must be filled");
+        }
+
+        req.setAttribute("songs", this.songService.getAllSongs());
         dispatcher.forward(req, resp);
     }
 }
